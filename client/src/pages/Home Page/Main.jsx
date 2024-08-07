2784461a1f423be79c1d238e198cbdd80169e094
Pankaj <pankajpriyadershi@gmail.com>
2024-08-08 02:41:46 +0530

    logo preview added and a new page AdminCreation is created by modifying signup window

--- client/src/pages/Home Page/Main.jsx
+++ client/src/pages/Home Page/Main.jsx
@@ -4,27 +4,54 @@ import { FaRegEye } from "react-icons/fa";
 import { FaEyeSlash } from "react-icons/fa";
 
 // Importing React Packages
-import { useEffect, useState } from "react"
-import { Link } from 'react-router-dom'
+import { useEffect, useRef, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
-import './Main.css'
+import "./Main.css";
 
 export default function Main() {
   // useState
-  const [companyDetails, setCompanyDetails] = useState([])
+  // const [companyDetails, setCompanyDetails] = useState([]);
   const [showPassword, setShowPassword] = useState(false);
+  const [logo, setLogo] = useState(null);
+
+  //useRef
+  const company = useRef();
+  const owner = useRef();
+
+  //useNavigate
+  const navigate = useNavigate();
 
   // useEffect
   useEffect(() => {
-    let cards = document.getElementById('card');
-    cards.onmousemove = function(e){
+    let cards = document.getElementById("card");
+    cards.onmousemove = function (e) {
       let x = e.pageX - cards.offsetLeft;
       let y = e.pageY - cards.offsetTop;
 
-      cards.style.setProperty('--x', x+'px');
-      cards.style.setProperty('--y', y+'px');
+      cards.style.setProperty("--x", x + "px");
+      cards.style.setProperty("--y", y + "px");
+    };
+  }, []);
+  //handle logo preview
+  const handlePreview = (event) => {
+    const file = event.target.files[0];
+    if (file) {
+      setLogo(file);
+    } else {
+      setLogo(null);
     }
-  }, [])
+  };
+  let companyDetails = {};
+  const handleNext = () => {
+    companyDetails = {
+      company: company.current.value,
+      owner: owner.current.value,
+      logo: logo,
+    };
+    console.log(companyDetails);
+    navigate("/create-admin", { state: companyDetails });
+  };
 
   return (
     <div className="text-lg text-white bg-[#222] flex justify-center items-center w-lvw min-h-lvh p-5">
@@ -33,33 +60,80 @@ export default function Main() {
         className="relative bg-[#2d2d2d] w-[30rem] h-[30rem] p-8 rounded-lg shadow-lg
           before:absolute before:top-[--y] before:left-[--x] before:content-[''] before:opacity-0 hover:before:opacity-100 before:rounded-full
           overflow-hidden"
-        >
-          <div className="z-20 relative font-bold text-xl text-black flex flex-col gap-10 w-full h-full rounded-full">
-            <section className="relative">
-              <input type="text" name="companyName" id="companyName" onChange={(e) => setCompanyDetails(prevValue => {return {...prevValue, company: e.target.value}})} className="peer w-full px-5 py-2 rounded-full outline-none" />
-              <label htmlFor="companyName" className={`absolute top-2 left-5 peer-focus:-top-4 peer-focus:left-4 peer-focus:backdrop-blur-sm peer-focus:rounded-full ${companyDetails.company && 'peer-valid:-top-4 peer-valid:left-4 peer-valid:backdrop-blur-sm peer-valid:rounded-full'} duration-700 cursor-pointer`}>Company Name</label>
-            </section>
+      >
+        {/* <div className="company-details">
+          <span>Company Details</span>
+        </div> */}
+        <div className="z-20 relative font-bold text-xl text-black flex flex-col gap-10 w-full h-full rounded-full">
+          <section className="relative">
+            <input
+              type="text"
+              name="companyName"
+              id="companyName"
+              ref={company}
+              className="peer w-full px-5 py-2 rounded-full outline-none"
+            />
+            <label
+              htmlFor="companyName"
+              className={`absolute top-2 left-5 peer-focus:-top-4 peer-focus:left-4 peer-focus:backdrop-blur-sm peer-focus:rounded-full ${
+                companyDetails.company &&
+                "peer-valid:-top-4 peer-valid:left-4 peer-valid:backdrop-blur-sm peer-valid:rounded-full"
+              } duration-700 cursor-pointer`}
+            >
+              Company Name
+            </label>
+          </section>
 
-            <section className="relative">
-              <input type="text" name="ownerName" id="ownerName" onChange={(e) => setCompanyDetails(prevValue => {return {...prevValue, owner: e.target.value}})} className="peer w-full px-5 py-2 rounded-full outline-none" />
-              <label htmlFor="ownerName" className={`absolute top-2 left-5 peer-focus:-top-4 peer-focus:left-4 peer-focus:backdrop-blur-sm peer-focus:rounded-full ${companyDetails.owner && 'peer-valid:-top-4 peer-valid:left-4 peer-valid:backdrop-blur-sm peer-valid:rounded-full'} duration-700 cursor-pointer`}>Owner Name</label>
-            </section>
+          <section className="relative">
+            <input
+              type="text"
+              name="ownerName"
+              id="ownerName"
+              ref={owner}
+              className="peer w-full px-5 py-2 rounded-full outline-none"
+            />
+            <label
+              htmlFor="ownerName"
+              className={`absolute top-2 left-5 peer-focus:-top-4 peer-focus:left-4 peer-focus:backdrop-blur-sm peer-focus:rounded-full ${
+                companyDetails.owner &&
+                "peer-valid:-top-4 peer-valid:left-4 peer-valid:backdrop-blur-sm peer-valid:rounded-full"
+              } duration-700 cursor-pointer`}
+            >
+              Owner Name
+            </label>
+          </section>
 
-            <section className="relative mx-auto">
-              <input
-                type="file"
-                name="logo"
-                id="logo"
-                className="hidden" />
-              <label htmlFor="logo" className="flex gap-5 items-center bg-white w-full px-8 py-2 rounded-md cursor-pointer">
-                <span>Import Your Logo</span>
-                <CiImport className="size-8" />
-              </label>
-            </section>
+          <section className="relative mx-auto">
+            <input
+              type="file"
+              name="logo"
+              id="logo"
+              onChange={handlePreview}
+              className="hidden"
+            />
+            <label
+              htmlFor="logo"
+              className="flex gap-5 items-center bg-white w-full px-8 py-2 rounded-md cursor-pointer"
+            >
+              <span>Import Your Logo</span>
+              <CiImport className="size-8" />
+            </label>
 
-            <Link to='/login' state={companyDetails} className="text-white bg-green-600 w-fit ml-auto mt-auto px-5 py-2 rounded-lg">Next</Link>
-          </div>
+            {logo ? (
+              <img src={URL.createObjectURL(logo)} className="logo-preview" />
+            ) : (
+              <p>not selected</p>
+            )}
+          </section>
+
+          <button
+            onClick={handleNext}
+            className="text-white bg-green-600 w-fit ml-auto mt-auto px-5 py-2 rounded-lg"
+          >
+            Next
+          </button>
         </div>
+      </div>
     </div>
-  )
+  );
 }
--- client/src/routes/Routes.jsx
+++ client/src/routes/Routes.jsx
@@ -1,84 +1,94 @@
 // Importing React Packages
-import React from 'react' 
-import ReactDOM from 'react-dom/client'
-import '../../public/assets/styles/index.css'
-import { RouterProvider, createBrowserRouter, Route, Navigate } from 'react-router-dom'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "../../public/assets/styles/index.css";
+import {
+  RouterProvider,
+  createBrowserRouter,
+  Route,
+  Navigate,
+} from "react-router-dom";
 
 // Importing Redux Configuration
-import {Provider} from 'react-redux'
-import {store} from '../Redux/Store/store'
+import { Provider } from "react-redux";
+import { store } from "../Redux/Store/store";
 
 // Importing Local Components
-import HomePage from '../pages/Home Page/Main'
-import Login from '../pages/Login-Signup/Login'
-import Signup from '../pages/Login-Signup/Signup'
-import Admin from '../pages/Admin/Admin'
-import MainPanel from '../pages/Admin/pages/MainPanel'
-import Customers from '../pages/Admin/pages/Customers'
-import Messages from '../pages/Admin/pages/Messages'
-import Products from '../pages/Admin/pages/Products'
-import Subscriptions from '../pages/Admin/pages/Subscriptions'
-import Broadcast from '../pages/Admin/pages/Broadcast'
-import Reports from '../pages/Admin/pages/Reports'
-import Settings from '../pages/Admin/pages/Settings'
+import HomePage from "../pages/Home Page/Main";
+import Login from "../pages/Login-Signup/Login";
+import Signup from "../pages/Login-Signup/Signup";
+import Admin from "../pages/Admin/Admin";
+import MainPanel from "../pages/Admin/pages/MainPanel";
+import Customers from "../pages/Admin/pages/Customers";
+import Messages from "../pages/Admin/pages/Messages";
+import Products from "../pages/Admin/pages/Products";
+import Subscriptions from "../pages/Admin/pages/Subscriptions";
+import Broadcast from "../pages/Admin/pages/Broadcast";
+import Reports from "../pages/Admin/pages/Reports";
+import Settings from "../pages/Admin/pages/Settings";
+import AdminCreation from "../pages/Home Page/AdminCreation";
 
 const router = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     element: <HomePage />,
   },
   {
-    path: 'login',
-    element: <Login />
+    path: "create-admin",
+    element: <AdminCreation />,
   },
   {
-    path: 'signup',
-    element: <Signup />
+    path: "login",
+    element: <Login />,
   },
   {
-    path: 'admin',
+    path: "signup",
+    element: <Signup />,
+  },
+  {
+    path: "admin",
     element: <Admin />,
     children: [
       {
         path: "",
-        element: <MainPanel />
+        element: <MainPanel />,
       },
       {
         path: "customers",
-        element: <Customers />
+        element: <Customers />,
       },
       {
         path: "messages",
-        element: <Messages />
+        element: <Messages />,
       },
       {
         path: "products",
-        element: <Products />
+        element: <Products />,
       },
       {
         path: "subscription",
-        element: <Subscriptions />
+        element: <Subscriptions />,
       },
       {
         path: "broadcast",
-        element: <Broadcast />
+        element: <Broadcast />,
       },
       {
         path: "reports",
-        element: <Reports />
+        element: <Reports />,
       },
       {
         path: "settings",
-        element: <Settings />
+        element: <Settings />,
       },
-    ]
-  }
-])
+    ],
+  },
+]);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
       <RouterProvider router={router} />
     </Provider>
   </React.StrictMode>
-)
+);
