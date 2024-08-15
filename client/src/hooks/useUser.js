3872ba3d3aeed8fa46b1494264915349401ceb0f
Pankaj <pankajpriyadershi@gmail.com>
2024-08-15 11:11:07 +0530

    usaved

--- client/src/hooks/useUser.js
+++ client/src/hooks/useUser.js
@@ -1,12 +1,18 @@
 // Importing React Packages
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 
 // Importing Redux files
-import { useDispatch } from 'react-redux';
-import { creatingInitialState } from '../Redux/features/UserSlice'
+import { useDispatch } from "react-redux";
+import { creatingInitialState } from "../Redux/features/UserSlice";
 
 // Importing Services
-import { signup as signupService, login as loginService, UserSearch, UserResetPassword, fetchUser } from '../services/userService'
+import {
+  signup as signupService,
+  login as loginService,
+  UserSearch,
+  UserResetPassword,
+  fetchUser,
+} from "../services/userService";
 
 export const useUser = () => {
   // useDispatch
@@ -17,32 +23,32 @@ export const useUser = () => {
 
   const signup = async (data) => {
     try {
-      const user = await signupService(data);    
-      
-      if(typeof user === 'string') return user;
-      
+      const user = await signupService(data);
+
+      if (typeof user === "string") return user;
+
       // setting localStorage variables
       localStorage.setItem("authToken", user.authToken);
-      
+
       return user.data;
     } catch (error) {
-      console.log("Error Signing Up in UseUser Hook: ", error); 
+      console.log("Error Signing Up in UseUser Hook: ", error);
     }
-  }
-  
+  };
+
   const login = async (data) => {
     try {
       const user = await loginService(data);
-  
-      if(typeof user === 'string') return user;
+
+      if (typeof user === "string") return user;
 
       // setting localStorage variables
-      localStorage.setItem("authToken", user.authToken); 
+      localStorage.setItem("authToken", user.authToken);
       return user.data;
     } catch (error) {
-      console.log("Error Logging In in UseUser Hook: ", error); 
+      console.log("Error Logging In in UseUser Hook: ", error);
     }
-  }
+  };
 
   const logout = async () => {
     try {
@@ -51,46 +57,46 @@ export const useUser = () => {
 
       await getUser();
 
-      navigate('/');
+      navigate("/");
     } catch (error) {
-      console.log("Error Logging Out in UseUser Hook: ", error); 
+      console.log("Error Logging Out in UseUser Hook: ", error);
     }
-  }
+  };
 
   const searchUser = async (user) => {
     try {
       const response = await UserSearch(user);
-      
+
       return response;
     } catch (error) {
-      console.log("Error Resetting password in UseUser Hook: ", error); 
+      console.log("Error Resetting password in UseUser Hook: ", error);
     }
-  }
+  };
 
   const resetPassword = async (data) => {
     try {
       const response = await UserResetPassword(data);
       return response;
     } catch (error) {
-      console.log("Error Resetting password in UseUser Hook: ", error); 
+      console.log("Error Resetting password in UseUser Hook: ", error);
     }
-  }
+  };
 
   const getUser = async () => {
     try {
       const id = localStorage.getItem("authToken");
 
-      if(id){
+      if (id) {
         const data = await fetchUser(id);
-        
+
         dispatch(creatingInitialState(data));
-      }else{
+      } else {
         dispatch(creatingInitialState({}));
       }
     } catch (error) {
-      console.log("Error Getting User:", error);   
+      console.log("Error Getting User:", error);
     }
-  }
+  };
 
-  return { signup, login, searchUser, resetPassword, logout, getUser }
-}
+  return { signup, login, searchUser, resetPassword, logout, getUser };
+};
